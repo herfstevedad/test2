79ecb394b07380e13e7f1375450aa50a4066e5a7
Неминущий Максим Иванович <neminuschiy@DRAFT.LOCAL>
2025-03-03 15:00:43 +0300

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css'; // Глобальные стили приложения
 import App from './App'; // Корневой компонент приложения
-import { BrowserRouter } from 'react-router-dom'; // Для маршрутизации (если используется)
+
 
 // Рендеринг приложения
 
   <React.StrictMode>
-    {/* Оборачиваем приложение в BrowserRouter, если используется навигация */}
-    <BrowserRouter>
       <App />
-    </BrowserRouter>
   </React.StrictMode>
