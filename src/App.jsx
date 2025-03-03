e17b0e09aeaee81719e637f9bd6d39a5a9d5829c
Неминущий Максим Иванович <neminuschiy@DRAFT.LOCAL>
2025-03-03 15:13:13 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,6 @@
-import React, { useEffect, useState } from 'react';
-import { initDataRaw, retrieveLaunchParams } from '@telegram-apps/sdk';
-import { addUserIfNotExists } from './other/addUser';
-import './App.css';
+import React, { useEffect, useRef } from 'react';
+import { WebAppProvider } from '@tinkoff/react-telegram-web-app';
 
-function App() {
-  const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const initApp = async () => {
-      try {
-        const initDataRaw = retrieveLaunchParams();
-        const telegramUserId = initDataRaw.tgWebAppData?.user?.id;
-        const telegramUsername = initDataRaw.tgWebAppData?.user?.username;
 
-        if (telegramUserId) {
-          const userData = {
-            username: telegramUsername || 'Unknown',
-            createdAt: new Date().toISOString(),
-          };
-          await addUserIfNotExists(telegramUserId, userData);
-        } else {
-          console.warn('User data is not available.');
-        }
-      } catch (error) {
-        console.error('Error initializing app:', error);
-      } finally {
-        setLoading(false); // Завершение загрузки
-      }
-    };
-
-    initApp();
-  }, []);
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
-  return (
-    <div className="App">
-      <h1>Welcome to the App</h1>
-      <p>User ID: {retrieveLaunchParams().tgWebAppData?.user?.id}</p>
-      <p>Username: {retrieveLaunchParams().tgWebAppData?.user?.username}</p>
-    </div>
-  );
-}
-
-export default App;
+export default Game;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css'; // Глобальные стили приложения
-import App from './App'; // Корневой компонент приложения
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
 
-// Рендеринг приложения
+import { init, miniApp, mainButton, shareURL } from '@telegram-apps/sdk';
 
-  <React.StrictMode>
-      <App />
-  </React.StrictMode>
+
+const initializeTelegramSDK = async () => {
+  try {
+    await init();
+
+
+    if (miniApp.ready.isAvailable()) {
+      await miniApp.ready();
+      console.log('Mini App готово');
+
+      miniApp.setHeaderColor('#fcb69f');
+    }
+
+
+  } catch (error) {
+    console.error('Ошибка инициализации:', error);
+  }
+};
+
+
+
+initializeTelegramSDK();
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
